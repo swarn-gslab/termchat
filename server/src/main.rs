c0d86d57a061e78dee348a673cc5635ba4f0778b
Bhupalji <bhupalprajapati12@gmail.com>
2024-03-16 13:19:13 +0530

    updated status of client

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,13 @@
 pub mod login;
 pub mod message;
-pub mod error;
+use axum::middleware::AddExtension;
 use axum::routing::get;
 // use axum::routing::get; TODO:
 use axum::Extension;
 use axum::{routing::post, Router};
-use login::{login, UserDatabase};
-use std::sync::Arc;
-use message::{send_message,get_receiver};
+use login::{login, UserDatabase,online_status};
+use message::{get_receiver_msg, send_message};
+use std::sync::{Arc, Mutex};
 
 use crate::message::InMemoryDatabase;
 
@@ -15,20 +15,23 @@ use crate::message::InMemoryDatabase;
 #[tokio::main]
 async fn main() {
     let user_db = Arc::new(UserDatabase::new());
+    // let user_database = Arc::new(Mutex::new(UserDatabase::new()));
     let db = Arc::new(InMemoryDatabase::new());
     let app = Router::new()
         .route("/login", post(login))
+        .route("/status", post(online_status))
         .layer(Extension(user_db))
-         // here we add more routers
+       
+        // .layer(Extension(user_database)) 
+        // here we add more routers
         .route("/send_message", post(send_message))
-        .route("/get_receiver/:sender", get(get_receiver))
+        .route("/get_receiver/:sender", get(get_receiver_msg))
         .layer(Extension(db));
-         
-         
-        // .route("/", get(health_check)) 
-        // TODO:
-        // .route("/message", get(create_message)) // TODO:
-        
+
+    // .route("/", get(health_check))
+    // TODO:
+    // .route("/message", get(create_message)) // TODO:
+
     // Start the server
     let lis = tokio::net::TcpListener::bind("0.0.0.0:3010").await.unwrap();
     let port = "3010";
